879191073002b8132b9bebac815de1dd1f597400
Aleksey Bormatov <inferio3@gmail.com>
2024-03-13 00:50:17 +0300

    add lambda

--- calc.py
+++ calc.py
@@ -1,2 +1,19 @@
-x = 9%3
-print(x)
+people = ['Антон', 'Соня', 'Коля', 'Женя', 'Тоня', 'Стёпа']
+
+def say_to_all(func, sequence):
+    for item in sequence:
+        func(item)
+
+
+# Этот вызов для каждого имени из списка должен напечатать
+# строчку Привет, <имя>!
+say_to_all(lambda a : print('Привет', a), people)
+# Этот вызов для каждого имени из списка должен напечатать
+# строчку До завтра, <имя>!
+#say_to_all(lambda b : f'До завтра, {b}!', people)
+say_to_all(lambda b : print('До завтра', b), people)
+
+#say_to_all(print, people)
+
+#a = 'Семен' 
+#print((lambda a : f'Привет, {a}!') ('Семен'))
