fcad22203af931dd62fae069fc0ff7ba2329fbc4
Aleksey Bormatov <inferio3@gmail.com>
2024-03-13 00:58:44 +0300

    add qution

--- calc.py
+++ calc.py
@@ -7,13 +7,14 @@ def say_to_all(func, sequence):
 
 # Этот вызов для каждого имени из списка должен напечатать
 # строчку Привет, <имя>!
-say_to_all(lambda a : print('Привет', a), people)
+say_to_all(lambda a : print('Привет, ' + a + '!'), people)
 # Этот вызов для каждого имени из списка должен напечатать
 # строчку До завтра, <имя>!
 #say_to_all(lambda b : f'До завтра, {b}!', people)
-say_to_all(lambda b : print('До завтра', b), people)
+say_to_all(lambda b : (f'До завтра, {b}!'), people)
 
 #say_to_all(print, people)
 
 #a = 'Семен' 
-#print((lambda a : f'Привет, {a}!') ('Семен'))
+x = (lambda a : f'Привет, {a}!') ('Семен')
+print(x)
